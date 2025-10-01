a9ef933ab103ae228326e0e7674d84d4174682c5
Mark Chen <9006264+mark9804@users.noreply.github.com>
2025-10-01 18:27:58 +0900

    chore: scaffold

--- eslint.config.js
+++ eslint.config.js
@@ -1,61 +1,67 @@
-import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
-import pluginVue from 'eslint-plugin-vue';
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
   {
-    files: ['src/**/*.{ts,tsx,vue}'],
+    files: ["src/**/*.{ts,tsx,vue}"],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.app.json'],
-        extraFileExtensions: ['.vue'],
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.app.json"],
+        extraFileExtensions: [".vue"],
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': 'warn',
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
   {
-    files: ['*.ts', '*.js'],
+    files: ["*.ts", "*.js"],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': 'warn',
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
-  ...pluginVue.configs['flat/recommended'],
+  ...pluginVue.configs["flat/recommended"],
   {
-    files: ['**/*.vue'],
+    files: ["**/*.vue"],
     languageOptions: {
       parserOptions: {
         parser: tsparser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
     },
     rules: {
-      'vue/multi-word-component-names': 'off',
+      "vue/multi-word-component-names": "off",
+      "vue/max-attributes-per-line": "off",
+      "vue/html-self-closing": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/html-closing-bracket-newline": "off",
     },
   },
   {
-    ignores: ['dist', 'node_modules', '.git'],
+    ignores: ["dist", "node_modules", ".git"],
   },
+  eslintConfigPrettier,
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.scss";
+import App from "./App.vue";
+import "virtual:uno.css";
+import "@unocss/reset/normalize.css";
 
-createApp(App).mount('#app');
+createApp(App).mount("#app");
